4718e6279dd1bc971d9e82f6b515636acb4a0968
shige-yuka <shige.yuka.p@gmail.com>
2018-09-27 02:39:47 +0900

    共通するuiを追加

---
+++ 08_17_TakeshigeYuka/bookmark/ui/globalnav.php
@@ -0,0 +1,15 @@
+<nav class="nav">
+  <div id="nav-drawer">
+      <input id="nav-input" type="checkbox" class="nav-unshown">
+      <label id="nav-open" for="nav-input"><span></span></label>
+      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+      <div id="nav-content">
+        <ul class="nav-list">
+          <li class="nav-item"><a class="nav-link" href="/bookmark/index.php">Bookmark登録</a></li>
+          <li class="nav-item"><a class="nav-link" href="/bookmark/select.php">Bookmark一覧</a></li>
+          <li class="nav-item"><a class="nav-link" href="/bookmark/user/select.php">ユーザー一覧</a></li>
+          <li class="nav-item"><a class="nav-link" href="/bookmark/user/signout.php">サインアウト</a></li>
+        </ul>
+      </div>
+  </div>
+</nav>
